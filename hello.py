5fb502fd1e47a25b2459c199699da10057d7d968
abdou <abdoulatufcolo@outlook.fr>
2025-09-22 16:00:22 +0200

    Feat: Correct to adapt to python conventions

--- hello.py
+++ hello.py
@@ -1,20 +1,25 @@
+# Show "Hello, World?"
 print("Hello, World?")
 
-#Create a Syntaxe error
-#print("Bonjour)
-#print(Bonjour")
+# Create a Syntaxe error
+# print("Bonjour)
+# print(Bonjour")
 
 # Create a runtime error NameError 
 # print(Bonjour)
 
-#Create the variable 
+# Create the variable 
 greeting = "Hello, World!"
 
-#print the content of the variable
+# print the content of the variable
 print(greeting)
 
+# Print without variable
 print("Hello, you are in the programming course cs312.")
+
+# Print with variable teacher_greet_students
 teacher_greet_students = "Hello, you are in the programming course cs312."
 print(teacher_greet_students)
 
+# Variable inspection 
 greeting
