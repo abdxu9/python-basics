7db6417906ddc119356d7aa272b48e6b7f84b117
abdou <abdoulatufcolo@outlook.fr>
2025-09-22 16:35:52 +0200

    Feat: Testing string implementation in python

---
+++ string.py
@@ -0,0 +1,26 @@
+print(type("Hello, World"))
+string1 = "Hello, World"
+print(len(string1))
+
+string2 = "This planet has—or rather had—a problem, which was this: most of the people living on it were unhappy for pretty much of the time. Many solutions were suggested for this problem, but most of these were largely concerned with the movements of small green pieces of paper, which is odd because on the whole it wasn’t the small green pieces of paper that were unhappy. —Douglas Adams, TheHitchhiker’s Guide to the Galaxy"
+print(len(string2))
+
+string3 = """This planet has—or rather had—a problem, which was
+ this: most of the people living on it were unhappy for
+ pretty much of the time. Many solutions were suggested
+ for this problem, but most of these were largely con
+cerned with the movements of small green pieces of
+ paper, which is odd because on the whole it wasn’t the
+ small green pieces of paper that were unhappy.
+ —Douglas Adams, TheHitchhiker’s Guide to the Galaxy"""
+print(len(string3))
+
+print(string2)
+print("\n")
+print(string3)
+
+print("He said: \"Hello everyone\" ")
+print("In french \"the man\" is translated as \"l\'homme\"")
+
+
+      
