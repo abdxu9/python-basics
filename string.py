4f42fa8dfc45ff9e6c9ac7ef9140b279e5fdec09
abdou <abdoulatufcolo@outlook.fr>
2025-09-22 17:02:35 +0200

    Feat: Saw some string operations and functions

--- string.py
+++ string.py
@@ -22,5 +22,59 @@ print(string3)
 print("He said: \"Hello everyone\" ")
 print("In french \"the man\" is translated as \"l\'homme\"")
 
+string1 = "Hello"
+string2 = "World"
+string3 = string1 + " " + string2 
+print(string3)
 
+print(string1[0])
+print(string2[0:3])
+
+print(string3.lower())
+print(string3.upper())
+
+string1 = "Animals"
+string2 = "Badger"
+string3 = "Honey Bee"
+string4 = "Honey Badger"
+
+print("\n")
+print(string1.lower())
+print("\n")
+print(string2.lower())
+print("\n")
+print(string3.lower())
+print("\n")
+print(string4.lower())
+
+print("\n")
+print(string1.upper())
+print(string2.upper())
+print("\n")
+print(string3.upper())
+print("\n")
+print(string4.upper())
+print("\n")
+
+string1 = "    Filet Migon"
+string2 = "Brisket    "
+string3 = "    Cheeseburger   "
+
+print(string1.lstrip()) 
+print(string2.rstrip())
+print(string3.strip())
+
+string1 = "Becomes"
+string2 = "becomes"
+string3 = "BEAR"
+string4 = "  bEautiful"
+
+print(string1.startswith("be"))  # False 
+print(string2.startswith("be"))  # True
+print(string3.startswith("be"))  # False
+print(string4.startswith("be"))  # False
+
+print(string1.lower().startswith("be"))
+print(string3.lower().startswith("be"))
+print(string4.lower().lstrip().startswith("be"))
       
