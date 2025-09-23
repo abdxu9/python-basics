2935406a18b013a59e1cc87bae9ac7674b26a5fc
abdou <abdoulatufcolo@outlook.fr>
2025-09-23 08:35:05 +0200

    Feat: Conversion of string to number.

---
+++ conversion.py
@@ -0,0 +1,24 @@
+string1 = "1"
+print(string1)
+
+print(f"string * 2 = {string1 * 2}")
+
+string1 = int(string1)
+print(string1)
+
+print(f"string * 2 = {string1 * 2}")
+
+string1 = float(string1)
+print(string1)
+
+print(f"string * 2 = {string1 * 2 }")
+
+string2 = "Bonjour a vous " + str(2) +  "."
+print(string2)
+
+first_number = input("Enter a number. ")
+second_number = input("Enter a second number. ")
+first_number = float(first_number)
+second_number = float(second_number)
+
+print(f"{first_number} * {second_number} = {first_number * second_number}.")
